2c719ad709ac985ca00836bf51566a895ce3e5f1
Femi401 <femiajayi12@outlook.com>
2021-06-16 00:33:57 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3289615d-6f54-41b7-beec-724b164e8f6e';
+export const MODAL_CLOSE_SEC = 2.5;
